ddd33120c7f2c89298f4188b272a6b0516cdb2a3
未知の侍 <morganmilstone983@gmail.com>
2026-05-14 20:22:13 +0300

    Add: command handling for 'init' in index.js

--- bin/index.js
+++ bin/index.js
@@ -2,6 +2,16 @@
 
 import React from "react";
 import { render } from "ink";
+import path from "path";
+import fs from "fs-extra";
+import process from "process";
 import App from "../core/tui.js";
+import InitApp from "../core/init.js";
 
-render(React.createElement(App));
+const command = process.argv[2];
+
+if (command === "init") {
+  render(React.createElement(InitApp));
+} else {
+  render(React.createElement(App));
+}
